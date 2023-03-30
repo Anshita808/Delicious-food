013467dc5011d0c970c4e503d133b8d4fbf54127
Anshita <anshitagupta783@gmail.com>
2023-03-30 20:49:34 +0530

    home page

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -5,32 +5,40 @@ let mainSection = document.getElementById("main");
 function renderCardList(cardData) {
     let cardList =
         `<div class="card-list">
+        
                 ${cardData
-            .map((item) =>
-                getCard(
-                    item.image,
-                    item.title,
-                    item.grams,
-                    item.price,
-                    item.time
+            .map((item, i) => {
 
-                )
-            )
-            .join("")}
+                if (i < 4) {
+                    return getCard(
+                        item.image,
+                        item.title,
+                        item.grams,
+                        item.price,
+                        item.time
+
+                    )
+                }
+            })
+            .join("")
+        }
+       
             </div>`
         ;
+    mainSection.innerHTML = cardList
 
-    mainSection.innerHTML = cardList;
+
 }
 
 function getCard(image, title, grams, price, time) {
     let card =
-        ` <div>
+        `
+        <div class="box">
         <img src="${image}" alt="">
         <button class="btn">+</button>
         <h3>${title}</h3>
         <p>${grams}</p>
-        <h3>${price}</h3>
+        <h4>${price}</h4>
         <p>${time}</p>
       </div>`
         ;
@@ -41,19 +49,20 @@ function getCard(image, title, grams, price, time) {
 
 
 
-function display(){
+function display() {
     fetch("http://localhost:8080/iftar")
-    .then((res)=>{
-        return res.json()
-    })
-    .then((data)=>{
-        console.log(data);
-        mainSection.innerHTML = null
-        renderCardList(data)
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            // console.log(data);
+            mainSection.innerHTML = null
+            renderCardList(data)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
 display()
 renderCardList()
+
